3ca6008225e384d38e45c33b31fc8731c46c9bd5
Dino <crescimbenidino@gmail.com>
2022-03-07 17:20:34 -0300

    Definición del modelo OrderDetails

--- api/models/index.js
+++ api/models/index.js
@@ -1,8 +1,9 @@
 const Category = require('./Category');
 const Product = require('./Product');
 const User = require('./User');
+const OrderDetails = require('./OrderDetails');
 
 // Product.hasMany(Category);
 // Category.hasMany(Product);
 
-module.exports = { Category, Product, User };
+module.exports = { Category, Product, User, OrderDetails };
