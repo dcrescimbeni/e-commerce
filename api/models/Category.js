f1778f83f00e3f3b4d86cbf9240620376f97951c
Dino <crescimbenidino@gmail.com>
2022-03-07 16:40:35 -0300

    Definición del modelo Categoría

---
+++ api/models/Category.js
@@ -0,0 +1,22 @@
+const { DataTypes, Model } = require('sequelize');
+const db = require('./_db');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  { sequelize: db, modelName: 'categories' }
+);
+
+module.exports = Category;
