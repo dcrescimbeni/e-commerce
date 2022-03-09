bcbd48755379b21431db2324417461259ad1a966
Dino <crescimbenidino@gmail.com>
2022-03-09 14:31:29 -0300

    hotfix-server-caido

--- api/routes/Users.js
+++ api/routes/Users.js
@@ -1,19 +1,21 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
-const UsersController = require('../controllers/UsersController')
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const UsersController = require('../controllers/UsersController');
 
+router.post('/register', UsersController.userCreate);
 
-router.post('/register',UsersController.userCreate)
+router.post(
+  '/login',
+  passport.authenticate('local'),
+  UsersController.userLogin
+);
 
-router.post('/login', passport.authenticate('local'), UsersController.userLogin)
+router.put('/edit/:id', UsersController.userEdit);
 
-router.put('/edit/:id' , UsersController.userEdit)
+router.delete('/admin/user/:id', UsersController.userDelete);
 
-router.delete('/admin/user/:id' , UsersController.userDelete)
+router.get('/admin/users', UsersController.getUsers);
+// router.put('/admin/user/:id', UsersController)
 
-router.get('/admin/users' , UsersController.getUsers)
-
-router.put('/admin/user/:id', UsersController)
-
-module.exports = router
+module.exports = router;
