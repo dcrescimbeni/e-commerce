db5e34cf46110ed71a309d6991f80550b651d845
Dino <crescimbenidino@gmail.com>
2022-03-09 17:56:23 -0300

    Agregado cors al server - resuelve bug que no permitia registrar

--- api/server.js
+++ api/server.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const volleyball = require('volleyball');
 const db = require('./models/_db');
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const passport = require('passport')
-const route = require("./routes")
-require('./config/auth')
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const route = require('./routes');
+const cors = require('cors');
+require('./config/auth');
 
 const app = express();
+
+app.use(cors());
 
 app.use(volleyball);
 app.use(express.json());
 
+app.use(cookieParser());
+app.use(session({ secret: 'bootcamp' }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(cookieParser())
-app.use(session({secret : 'bootcamp'}))
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use('/api',route)
+app.use('/api', route);
 
 app.use((err, req, res, next) => {
   console.log('Error');
@@ -26,8 +28,7 @@ app.use((err, req, res, next) => {
   res.status(500).send(err.message);
 });
 
-
-db.sync({ force:false }).then(() => {
+db.sync({ force: false }).then(() => {
   app.listen(3001, () => {
     console.log(`Server up on port 3001`);
   });
