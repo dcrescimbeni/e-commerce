aa608bd2fe434e6fbd9a562329cc605f7eb2b3ed
Dino <crescimbenidino@gmail.com>
2022-03-09 18:21:58 -0300

    Implementado search del producto

--- api/controllers/ProductsController.js
+++ api/controllers/ProductsController.js
@@ -1,52 +1,66 @@
-const Products = require("../models/Product")
+const Products = require('../models/Product');
+const { Op } = require('sequelize');
 
-exports.allProducts = (req,res) => {
-    Products.findAll()
-    .then(products => res.send(products))
-    .then(()=> res.send(200))
-    .catch(err => console.log(err))
-}
+exports.allProducts = (req, res) => {
+  Products.findAll()
+    .then((products) => res.send(products))
+    .then(() => res.send(200))
+    .catch((err) => console.log(err));
+};
 
-exports.productFind = (req,res) => {
-    Products.findOne(req.body, {
-        where:{
-            id : req.params.id
-            }
-        })
-    .then(()=> res.send(200))
-    .catch(err => console.log(err))
-}
+exports.productFind = (req, res) => {
+  Products.findOne(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => res.send(200))
+    .catch((err) => console.log(err));
+};
 
-exports.allProductsWithTag = (req,res) => {
-    Products.findAll(...tag, {
-        where:{
-            tag: req.params.tag
-        }
-    })
-    .then(()=> res.send(200))
-    .catch(err => console.log(err))
-}
+// exports.allProductsWithTag = (req,res) => {
+//     Products.findAll(...tag, {
+//         where:{
+//             tag: req.params.tag
+//         }
+//     })
+//     .then(()=> res.send(200))
+//     .catch(err => console.log(err))
+// }
 
-exports.newProduct = (req,res) => {
-    Products.create(req.body)
+exports.newProduct = (req, res) => {
+  Products.create(req.body)
     .then(() => res.send(201))
-    .catch(err => console.log(err))
-}
+    .catch((err) => console.log(err));
+};
 
 exports.editProduct = (req, res) => {
-    Products.update(req.body, req.body, {
-        where:{
-            id : req.params.id
-            }
-        })
-    .then(()=> res.send(204))
-    .catch(err => console.log(err))
-}
+  Products.update(req.body, req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => res.send(204))
+    .catch((err) => console.log(err));
+};
 
 exports.deleteProduct = (req, res) => {
-    Products.destroy({where :{
-        id : req.params.id
-    }})
-    .then(()=> res.send(204))
-    .catch(err => console.log(err))
-}
+  Products.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then(() => res.send(204))
+    .catch((err) => console.log(err));
+};
+
+exports.searchProduct = (req, res) => {
+  let searchQuery = req.query.search.toLowerCase();
+
+  Products.findAll({
+    where: { name: { [Op.like]: `%${searchQuery}%` } },
+  })
+    .then((res) => res.dataValues)
+    .then((products) => res.send(products))
+    .catch((err) => console.log(err));
+};
--- api/routes/Products.js
+++ api/routes/Products.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const passport = require('passport')
-const router = express.Router()
-const ProductsController = require("../controllers/ProductsController")
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+const ProductsController = require('../controllers/ProductsController');
 
+router.get('/allProducts', ProductsController.allProducts);
 
-router.get("/allProducts",ProductsController.allProducts)
+router.get('/product/:id', ProductsController.productFind);
 
-router.get("/product/:id" , ProductsController.productFind)
+router.get('/tag/:tag', ProductsController.allProductsWithTag);
 
-router.get("/tag/:tag", ProductsController.allProductsWithTag)
+router.post('/newProduct', ProductsController.newProduct);
 
-router.post("/newProduct",ProductsController.newProduct)
+router.put('/product/:id', ProductsController.editProduct);
 
-router.put("/product/:id", ProductsController.editProduct)
+router.delete('/product/:id', ProductsController.deleteProduct);
 
-router.delete("/product/:id", ProductsController.deleteProduct)
+router.get('/search', ProductsController.searchProduct);
 
-module.exports = router
+module.exports = router;
