076af69cc84ae3c673f592a2af7abb9f7012fbb5
FacundoMuller93 <facundomuller@gmail.com>
2022-03-09 11:32:26 -0300

    product grid y product view sin estilos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ProductList from "./components/ProductList";
+import ProductDetails from "./components/ProductDetails";
+import { Routes, Route } from "react-router-dom";
+import data from "./fakeDB/data";
 
 function App() {
+  const { products } = data;
+
+  // useEffect(() => {
+  //   setProductsList(products);
+  //   console.log(products);
+  // }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <Routes>
+      <Route path={"/"} element={<ProductList products={products}/>} />
+        <Route path={`/products/:productId`} element={<ProductDetails />} />
+      </Routes>
     </div>
   );
 }
