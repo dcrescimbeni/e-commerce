286b3f3d17bdc00dc25e1cfbf62aa2a157e28d3c
Dino <crescimbenidino@gmail.com>
2022-03-14 15:22:08 -0300

    Terminado isAuth y isAdmin utility functions

--- api/utils/authCheck.js
+++ api/utils/authCheck.js
@@ -1,5 +1,5 @@
 exports.isAuth = (req, res, next) => {
-  if (req.user) return next();
+  if (req.isAuthenticated()) return next();
   else {
     let err = new Error('User is not authenticated');
     next(err);
@@ -7,7 +7,6 @@ exports.isAuth = (req, res, next) => {
 };
 
 exports.isAdmin = (req, res, next) => {
-  // console.log('Admin check');
   if (req.user.dataValues.isAdmin) return next();
   else {
     let err = new Error('User is not an admin');
