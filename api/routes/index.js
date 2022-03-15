7d4e0647921c44cb475356ee12bc8ffc4ce78901
Dino <crescimbenidino@gmail.com>
2022-03-15 13:12:21 -0300

    Fixed merging

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const product = require('./Products');
 const user = require('./Users');
