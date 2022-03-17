93dcb81cdc55326323fcb6658e6234657ffe50ea
reynaldovalencia <reynaldovalencia@gmail.com>
2022-03-17 12:39:29 -0600

    Checkout v3

--- src/state/store.js
+++ src/state/store.js
@@ -1,14 +1,19 @@
-import  {configureStore} from '@reduxjs/toolkit'
-import  logger from "redux-logger";
-import {userReducer} from './user'
+import { configureStore } from '@reduxjs/toolkit'
+import logger from "redux-logger";
+
+import { userReducer } from './user';
+import { productsReducer } from './products';
+import { ordersReducer } from './orders';
 
 
 const store = configureStore({
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
     reducer: {
-        user:userReducer,
-    
+        user: userReducer,
+        products: productsReducer,
+        order: ordersReducer,
+
     },
-});  
+});
 
 export default store;
