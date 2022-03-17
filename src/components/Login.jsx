df3bab54397aaa281efb360507f3a2dd3a9acdfc
Dino <crescimbenidino@gmail.com>
2022-03-17 19:59:30 -0300

    Finished user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,23 +3,22 @@ import { Link, useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import useInput from '../Hooks/useInputs';
-import { sendLogoutRequest } from "../state/user";
+import { sendLogoutRequest } from '../state/user';
 import { sendLoginRequest } from '../state/user';
 import { FcGoogle } from 'react-icons/fc';
 import { BsFacebook } from 'react-icons/bs';
-import { GiConverseShoe } from "react-icons/gi"
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import { CgProfile } from "react-icons/cg";
+import { GiConverseShoe } from 'react-icons/gi';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+import { CgProfile } from 'react-icons/cg';
 import style from '../styles/Login.module.css';
 
 const Login = () => {
-
   const inputEmail = useInput();
   const inputPassword = useInput();
 
   const dispatch = useDispatch();
   let navigate = useNavigate();
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const handleClick = () => {
     dispatch(sendLogoutRequest());
@@ -36,10 +35,8 @@ const Login = () => {
   };
 
   const handleSearchSubmit = async (e) => {
-
-    e.preventDefault()
-    navigate(`/search?query=${searchTerm}`)
-  
+    e.preventDefault();
+    navigate(`/search?query=${searchTerm}`);
   };
 
   // const Submit = (e) => {
@@ -53,165 +50,159 @@ const Login = () => {
   });
 
   return (
-    
-    <div className={style.masthead}>  
+    <div className={style.masthead}>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
+        <div className="container ">
+          <li className="nav-item d-flex">
+            <Link to="/">
+              <a className="navbar-brand">
+                <GiConverseShoe size={30} />
+                SNikers
+              </a>
+            </Link>
+          </li>
 
-<nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top ">
-      <div className="container ">
-        <li className="nav-item d-flex">
-          <Link to="/">
-            <a className="navbar-brand" >
-              <GiConverseShoe size={30} />
-              SNikers
-            </a>
-          </Link>
-        </li>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+              {/* Drop down Categories */}
 
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-           
+              {/* Search */}
+              <form
+                onSubmit={(e) => {
+                  handleSearchSubmit(e);
+                }}
+                className="d-flex"
+              >
+                <input
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                  className="form-control me-2"
+                  aria-label="Search"
+                  hint="Search"
+                  type="text"
+                  placeholder="Search"
+                />
+              </form>
+              {/* Search End*/}
 
-            {/* Drop down Categories */}
-           
+              <li className="nav-item">
+                <Link className="nav-link" to="/shoppingcart">
+                  {' '}
+                  <AiOutlineShoppingCart size={25} color="black" />
+                </Link>
+              </li>
 
-            {/* Search */}
-            <form onSubmit={(e) => {handleSearchSubmit(e)}} className="d-flex">
-              <input 
-                onChange={(e) => setSearchTerm(e.target.value)}
-                className="form-control me-2"
-                aria-label="Search"
-                hint="Search" 
-                type="text"
-                placeholder="Search"
-              />
-            </form>
-            {/* Search End*/}
+              {/* Profile Register/Login*/}
 
-            <li className="nav-item">
-              <Link className="nav-link" to="/shoppingcart">
-                {" "}
-                <AiOutlineShoppingCart size={25} color="black" />
-              </Link>
-            </li>
-
-            {/* Profile Register/Login*/}
-
-            {/* {user.adminId? ( <Link to="/admin">Admin</Link>):(
+              {/* {user.adminId? ( <Link to="/admin">Admin</Link>):(
               otra cosa
              )} */}
-           
 
-            {user.userId ? (
-              <>
-                {/* Test menu dropdown user logueado */}
-                 {/* Test menu dropdown user logueado */}
+              {user.userId ? (
+                <>
+                  {/* Test menu dropdown user logueado */}
+                  {/* Test menu dropdown user logueado */}
 
-                 <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <CgProfile size={25} color="black"/>{" "}{" "}
-                  <b>{user.firstName}</b>
-                 
-                </a>
+                  <li className="nav-item dropdown">
+                    <a
+                      className="nav-link dropdown-toggle"
+                      id="navbarDropdown"
+                      role="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                      <CgProfile size={25} color="black" />{' '}
+                      <b>{user.firstName}</b>
+                    </a>
 
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li>
-                    <Link to="/profile">
-                      <a className="dropdown-item" >
-                        Profile
-                      </a>
-                    </Link>
+                    <ul
+                      className="dropdown-menu"
+                      aria-labelledby="navbarDropdown"
+                    >
+                      <li>
+                        <Link to="/profile">
+                          <a className="dropdown-item">Profile</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <Link to="/purchaseHistory">
+                          <a className="dropdown-item">Purchase History</a>
+                        </Link>
+                      </li>
+                      <li>
+                        <li class="dropdown-divider" />
+                        <Link to="/">
+                          <button className="dropdown-item">Logout</button>
+                        </Link>
+                      </li>
+                    </ul>
                   </li>
-                  <li>
-                    <Link to="/purchaseHistory">
-                      <a className="dropdown-item" >
-                        Purchase History
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                  <li  class="dropdown-divider" />
-                    <Link to="/">
-                    <button  className="dropdown-item">
-                      Logout
-                    </button>
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              </>    
-            ) : (
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="#"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <CgProfile size={25} color= "black"/>
-                </a>
+                </>
+              ) : (
+                <li className="nav-item dropdown">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    href="#"
+                    id="navbarDropdown"
+                    role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    <CgProfile size={25} color="black" />
+                  </a>
 
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <Link to="/register">
-                      <a className="dropdown-item" href="#">
-                        Register
-                      </a>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link to="/login">
-                      <a className="dropdown-item" href="#">
-                        Login
-                      </a>
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-            )}
-          
-          </ul>
-          {/* Profile End */}
-        {user.isAdmin?(
-          <div>
-          <Link to="/admin">Admin</Link>
-          </div>
-        ):(console.log("Hola"))}
-       
-       <div>
-          <Link to="/admin">
-            <Button>
-             Admin
-            </Button>
-           
-          </Link>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="navbarDropdown"
+                  >
+                    <li>
+                      <Link to="/register">
+                        <a className="dropdown-item" href="#">
+                          Register
+                        </a>
+                      </Link>
+                    </li>
+                    <li>
+                      <Link to="/login">
+                        <a className="dropdown-item" href="#">
+                          Login
+                        </a>
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+              )}
+            </ul>
+            {/* Profile End */}
+
+            <div>
+              {user.isAdmin ? (
+                <Link to="/admin">
+                  <Button>Admin</Button>
+                </Link>
+              ) : (
+                <></>
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
 
-    <br/><br/><br/>
- 
+      <br />
+      <br />
+      <br />
+
       <div className="color-overlay d-flex justify-content-center align-items-center">
         <div className="containerForm">
           <Form onSubmit={handleSubmit} className="rounded p-4 p-sm-3">
@@ -249,7 +240,9 @@ const Login = () => {
             <br></br>
             <p>You don't have an account?</p>
             <Link to="/register">
-            <div className="d-flex justify-content-center"><b>Register</b></div> 
+              <div className="d-flex justify-content-center">
+                <b>Register</b>
+              </div>
             </Link>
             <Link to="/google">
               <FcGoogle size={32} />
