006cdae213a43857db0fd8ed33aeb15610503492
Dino <crescimbenidino@gmail.com>
2022-03-18 11:24:04 -0300

    El carrito de compras se vacia despues del checkout

--- src/Pages/Thanks.jsx
+++ src/Pages/Thanks.jsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import NavBar from "../components/NavBar";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import NavBar from '../components/NavBar';
 
-const Thanks = () => {
+const Thanks = ({ cartItems, setCartItems }) => {
+  const handleStorage = () => {
+    localStorage.removeItem('cart-products');
+    setCartItems([]);
+  };
 
-  const handleStorage = () =>{
-
-    localStorage.removeItem('cart-products')
-  }
-
+  useEffect(() => {
+    handleStorage();
+  }, []);
 
   return (
     <div>
@@ -21,26 +23,41 @@ const Thanks = () => {
           <div className="jumbotron text-center">
             <h1 className="display-3">Thanks For Your Order!</h1>
             <p className="lead">
-              <strong>Please check your email, 
-                  <br/>
-              </strong> 
-                  to see the details of your order.
+              <strong>
+                Please check your email,
+                <br />
+              </strong>
+              to see the details of your order.
             </p>
             <hr />
             <p>
               Problems? <Link to="/"> Contac Us</Link>
             </p>
             <p className="lead">
-              <Link to="/" className="btn btn-primary btn-sm" role="button" onClick={handleStorage}>
+              <Link
+                to="/"
+                className="btn btn-primary btn-sm"
+                role="button"
+                onClick={handleStorage}
+              >
                 Keep Buying
               </Link>
             </p>
           </div>
         </div>
       </section>
-      <br/><br/><br/><br/><br/>
-      <br/><br/><br/><br/><br/>
-      <br/><br/>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 };
